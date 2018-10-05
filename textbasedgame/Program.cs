8835c69062aef1de33c0b42d57bc9fd5138f45eb
alan audivert <alan.audivert@hotmail.fr>
2018-10-05 19:26:06 +0200

    still initialising

--- textbasedgame/Program.cs
+++ textbasedgame/Program.cs
@@ -6,10 +6,169 @@ using System.Threading.Tasks;
 
 namespace textbasedgame
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public class Character
         {
+            public string Name;
+            public int Health; // delcaring variables
+            public int Damage;
+            public int Mitigation;
+            public Character(string givenName, int givenHealth, int givenDamage, int givenMitigation)
+            {
+                this.Name = givenName;
+                this.Health = givenHealth;
+                this.Damage = givenDamage;
+                this.Mitigation = givenMitigation;
+
+            }
+
+
+        }
+
+        public class Monster
+        {
+            public string Name;
+            public int Health;
+            public int Damage;
+            public int Mitigation;
+            public Monster(string givenName, int givenHealth, int givenDamage, int givenMitigation)
+            {
+                this.Name = givenName;
+                this.Health = givenHealth;
+                this.Damage = givenDamage;
+                this.Mitigation = givenMitigation;
+            }
+
+            public static void Main()
+            {
+                Character userCharacter = new Character("placeholder", 0, 0, 0);
+                Monster newMonster = new Monster("placeholder", 0, 0, 0);
+
+
+                Console.WriteLine("What is your name?");
+                string userName = Console.ReadLine();
+                Console.WriteLine("So your name is " + userName + ", sounds gay");
+                // declaring struct name
+                Console.WriteLine("Please choose Wizard or Paladan");
+                string usercharchoice = Console.ReadLine();
+                bool wizard = usercharchoice == "wizard";
+                bool paladin = usercharchoice == "paladin";
+                while (wizard == false && paladin == false)
+                {
+                    Console.WriteLine("Invalid Selection");
+                    usercharchoice = Console.ReadLine();
+                    paladin = usercharchoice == "paladin";
+                    wizard = usercharchoice == "wizard";
+                }
+
+                if (paladin == true)
+                {
+                    userCharacter = new Character("Paladin", 120, 8, 4);
+                }
+                else if (wizard == true)
+                {
+                    userCharacter = new Character("Wizard", 75, 15, 2);
+                }
+
+
+                Console.WriteLine("Please enter command:");
+                Console.WriteLine("stats - your stats will be displayed.");
+                Console.WriteLine("hp - your health will be displayed.");
+                Console.WriteLine("dmg - your damage stat will be displayed.");
+                Console.WriteLine("def - your mitigation stat will be displayed.");
+                Console.WriteLine("continue - you will continue to the game.");
+
+
+
+                while (true)
+                {
+                    string command = Console.ReadLine();
+                    if (command == "stats")
+                    {
+                        Console.WriteLine(userCharacter.Health);
+                        Console.WriteLine(userCharacter.Damage);
+                        Console.WriteLine(userCharacter.Mitigation);
+                    }
+                    else if (command == "hp")
+                    {
+                        Console.WriteLine(userCharacter.Health);
+                    }
+                    else if (command == "dmg")
+                    {
+                        Console.WriteLine(userCharacter.Damage);
+                    }
+                    else if (command == "def")
+                    {
+                        Console.WriteLine(userCharacter.Mitigation);
+                    }
+                    else if (command == "continue")
+                    {
+                        Random rnd = new Random();
+                        int encounterType = rnd.Next(1, 2);
+                        int monsterType = rnd.Next();
+                        int eventDice = rnd.Next();
+                        if (encounterType == 1)
+                        {
+                            monsterType = rnd.Next(1, 5);
+                            if (monsterType == 1)
+                            {
+                                newMonster = new Monster("Ghoul", 60, 5, 3);
+                            }
+                            else if (monsterType == 2)
+                            {
+                                newMonster = new Monster("Ghost", 30, 9, 1);
+                            }
+                            else if (monsterType == 3)
+                            {
+                                newMonster = new Monster("Skeleton", 25, 11, 2);
+                            }
+                            else if (monsterType == 4)
+                            {
+                                newMonster = new Monster("Troll", 80, 3, 5);
+                            }
+                            else if (monsterType == 5)
+                            {
+                                newMonster = new Monster("gay retard", 10000, 69, 1337);
+                            }
+
+                            Console.WriteLine("Oh no there is a spooky monster! It's a " + newMonster.Name);
+                            Console.WriteLine("What will you do?");
+                            Console.WriteLine("Run Fight Sneak Playdead Examine");
+                            command = Console.ReadLine();
+                            if (command == "Examine")
+                            {
+                                Console.WriteLine(newMonster.Name);
+                                Console.WriteLine(newMonster.Health);
+                                Console.WriteLine(newMonster.Damage);
+                                Console.WriteLine(newMonster.Mitigation);
+                            }
+                            else
+                            {
+                                command = Console.ReadLine();
+                            }
+                        }
+
+                        if (encounterType == 2)
+                        {
+                            Console.WriteLine("This room is seems awfully suspicious");
+                            Console.WriteLine("What will you do?");
+                        }
+
+                        if (encounterType == 3)
+                        {
+                            Console.WriteLine("Treasure room how wonderful!");
+                            Console.WriteLine("What will you do?");
+                        }
+
+                        if (encounterType == 4)
+                        {
+                            Console.WriteLine("This looks likea good place to rest.");
+                            Console.WriteLine("What will you do?");
+                        }
+                    }
+                }
+            }
         }
     }
 }
